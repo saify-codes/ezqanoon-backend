dd02a7708662fa1825d340de749fc02b6e149c76
saify <dev.muhammadsarfaraz@gmail.com>
2025-03-22 20:51:13 +0500

    Commented out the "Edit Profile" and "Switch User" options in the lawyer header for potential future use, improving code maintainability.

--- resources/views/lawyer/layout/partials/header.blade.php
+++ resources/views/lawyer/layout/partials/header.blade.php
@@ -57,7 +57,7 @@
                                 <span>Profile</span>
                             </a>
                         </li>
-                        <li class="dropdown-item py-2">
+                        {{-- <li class="dropdown-item py-2">
                             <a href="javascript:;" class="text-body ms-0">
                                 <i class="me-2 icon-md" data-feather="edit"></i>
                                 <span>Edit Profile</span>
@@ -68,7 +68,7 @@
                                 <i class="me-2 icon-md" data-feather="repeat"></i>
                                 <span>Switch User</span>
                             </a>
-                        </li>
+                        </li> --}}
                         <li class="dropdown-item py-2">
                             <a href="{{ route('lawyer.signout') }}" class="text-body ms-0">
                                 <i class="me-2 icon-md" data-feather="log-out"></i>
